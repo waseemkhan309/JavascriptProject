ec7a712d6f65af521335fa6782361a65426038c1
WASEEM KHAN <waseem3khan333@gmail.com>
2024-01-30 02:00:22 +0500

    Model-Project

---
+++ Model/script.js
@@ -0,0 +1,28 @@
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const CloseModal = document.querySelector(".close-modal");
+const OpenModal = document.querySelectorAll(".show-modal");
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < OpenModal.length; i++)
+  OpenModal[i].addEventListener("click", openModal);
+
+CloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  // console.log(e.key);
+
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
